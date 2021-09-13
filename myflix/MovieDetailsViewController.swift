be66c448469ead160b0f1cf7b3f6daf62dbb1cf6
Luu <loluu@lib310387.ttu.edu>
2021-09-13 17:39:50 -0500

    w2-step1-c

---
+++ myflix/MovieDetailsViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MovieDetailsViewController.swift
+//  myflix
+//
+//  Created by Luu, Loc on 9/13/21.
+//
+
+import UIKit
+import AlamofireImage
+class MovieDetailsViewController: UIViewController {
+    
+    var movie: [String:Any]!
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //print out the titile to the console to test
+        print(movie["title"])
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        dateLabel.text = movie["release_date"] as? String
+        dateLabel.sizeToFit()
+        let baseUrl = "https://image.tmdb.org/t/p/w185/"
+        let postePpath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + postePpath)
+
+        posterView.af.setImage(withURL: posterUrl!)
+        
+        let backdropPpath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780/" + backdropPpath)
+                
+        backdropView.af.setImage(withURL: backdropUrl!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- myflix/MoviesViewController.swift
+++ myflix/MoviesViewController.swift
@@ -60,19 +60,31 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let postePpath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + postePpath)
                 
-        cell.posterView.af_setImage(withURL: posterUrl!)
+        cell.posterView.af.setImage(withURL: posterUrl!)
         return cell
       
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        print("Loading up the deatais screen")
+        //Find  the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        //turn off high light when click(moives) back to list movies
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }

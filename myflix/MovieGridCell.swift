e2d2c4492b7ad9c01f2031eb18fe1bf382a6731d
Luu <loluu@lib310376.ttu.edu>
2021-09-15 18:10:01 -0500

    step2 adding tab bar button

---
+++ myflix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  myflix
+//
+//  Created by Luu, Loc on 9/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
